73e14efe5dfb77ad8d8a37aac9aa06cd4650aff1
RamyashreeS11 <>
2021-05-27 17:39:54 +0000

    Program_5.c

--- Program_5.c
+++ Program_5.c
@@ -1,17 +1,24 @@
 //You will be given an integer array 'arr'. Your task is to print the largest and smallest element in 'arr'.
 
-
+ //Write your code here
 #include<stdio.h>
-
 int main()
 {
-    int arr[10], n, i;
+    int arr[10], n, i,large,small;
+    printf("\nEnter the number of elements :" );
     scanf("%d", &n);
-
-    for(i = 0; i < n; i++)
+    printf("\nInput the array elements : ");
+     for(i = 0; i < n; i++)
         scanf("%d", &arr[i]);
+        large=small=arr[0];
+        for(i=1;i<n;++i)
+       {
+       if(arr[i]>large)
+        large=arr[i];
 
-    //Write your code here
-  
+       if(arr[i]<small)
+       small=arr[i];
+      }
+      printf("\nThe smallest element is %d\n",small);printf("\nThe largest element is %d\n",large);
     return 0;
 }
