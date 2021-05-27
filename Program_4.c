0a4ce5d0ca94252f43f6106d4b2e933c115b03b7
RamyashreeS11 <>
2021-05-27 13:08:41 +0000

    Program_4.c

--- Program_4.c
+++ Program_4.c
@@ -7,6 +7,19 @@
 #include <stdio.h>
 int main()
 {
+  int N, a[10], i,sum=0;
+  printf("Enter the number of array elements ");
+  scanf("%d",&N);
+  printf("Enter the array elements") ;
+  for(i=0;i<N;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<N;i++)
+  {
+    sum=sum+a[i];
+  }
+  printf("The sum= %d",sum);
   //Declare all the variables[N, array a(1D array), i, sum=0] used in the code
   //Read the Value of array size N using scanf statement
   //Read the array elements using for loop and scanf statment
